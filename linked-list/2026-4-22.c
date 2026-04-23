f2467dff6d07256cde8c3249be5d973e7a21e73b
AsleepDragon <3120015775@qq.com>
2026-04-23 16:32:56 +0800

    2026-4-23

--- linked-list/2026-4-22.c
+++ linked-list/2026-4-22.c
@@ -88,6 +88,25 @@ int Print_list(node* L){
     return 1;
 }
 
+int list_len(node *L)
+{
+    if (L == NULL)
+    {
+        printf("Nonexistent linked list!\n");
+        return -1;
+    }
+
+    int count = 0;
+    node *count_nodes = L;
+    while (count_nodes->next != NULL)
+    {
+        count++;
+        count_nodes = count_nodes->next;
+    }
+
+    return count;
+}
+
 int insert_posion(node* L,int pos,int new_data){
     if(L == NULL){
         printf("Nonexsitent linked list!\n");
@@ -100,29 +119,31 @@ int insert_posion(node* L,int pos,int new_data){
             int ret = insert_tail(L,new_data);
             return ret=1?1:0;
         }
-        pirntf("Invalid position!\n");
+        printf("Invalid position!\n");
         return 0;
     }
 
-    
-}
-
-int list_len(node* L){
-    if(L == NULL){
-        printf("Nonexistent linked list!\n");
-        return -1;
+    node* front_node = L;
+    while(--pos){
+        front_node = front_node->next;
     }
 
-    int count = 0;
-    node* count_nodes = L;
-    while(count_nodes->next != NULL){
-        count++;
-        count_nodes = count_nodes->next;
+    node* new_node = (node*)malloc(sizeof(node));
+    if(new_node == NULL){
+        printf("Malloc failed!\n");
+        return 0;
     }
 
-    return count;
-}
+    new_node->data = new_data;
+    new_node->next = front_node->next;
+    new_node->prev = front_node;
+    if(front_node->next != NULL){
+        front_node->next->prev = new_node;
+    }
+    front_node->next = new_node;
 
+    return 1;
+}
 
 int main(){
     node* list = init_list();
